75c9958d22a2368843434eb0686c2f1893a99bd9
Libre Lol <hello@libre.lol>
2024-10-25 16:47:59 -0400

    Add categories.json file with social links, proxies, recommended services, tools, and notable mentions

--- src/components/LinkWidgets.vue
+++ src/components/LinkWidgets.vue
@@ -1,14 +1,25 @@
 <template>
-    <v-container>
-        <v-row>
+    <v-container fluid>
+        <v-row class="mb-4">
+            <v-col cols="12" class="d-flex justify-center">
+                <v-text-field
+                    v-model="searchQuery"
+                    placeholder="Search..."
+                    class="search-bar"
+                    solo
+                ></v-text-field>
+            </v-col>
+        </v-row>
+        <v-row class="d-flex justify-center" :class="{ 'scrollable': filteredCategories.length >= 3 }">
             <v-col 
-                v-for="(category, index) in categories" 
+                v-for="(category, index) in filteredCategories" 
                 :key="index" 
                 cols="12" 
                 md="4"
+                class="d-flex justify-center"
             >
-                <v-card elevation="2" class="pa-4 mb-4 card-animated">
-                    <v-card-title class="font-weight-bold text-center">
+                <v-card elevation="2" class="formatted-card">
+                    <v-card-title class="text-center font-weight-bold">
                         {{ category.title }}
                     </v-card-title>
                     <v-divider></v-divider>
@@ -19,7 +30,7 @@
                             :href="link.url"
                             target="_blank"
                             rel="noopener noreferrer"
-                            class="list-item-animated"
+                            class="link-item"
                         >
                             <v-list-item-icon>
                                 <v-icon 
@@ -37,8 +48,12 @@
                                 />
                             </v-list-item-icon>
                             <v-list-item-content>
-                                <v-list-item-title class="font-weight-semibold">{{ link.name }}</v-list-item-title>
-                                <v-list-item-subtitle class="subtitle">{{ link.description }}</v-list-item-subtitle>
+                                <v-list-item-title class="font-weight-semibold">
+                                    {{ link.name }}
+                                </v-list-item-title>
+                                <v-list-item-subtitle class="subtitle">
+                                    {{ link.description }}
+                                </v-list-item-subtitle>
                             </v-list-item-content>
                         </v-list-item>
                     </v-list>
@@ -49,163 +64,61 @@
 </template>
 
 <script>
+import categories from '../categories.json'; // Adjust the path as necessary
+
 export default {
     name: 'Links',
     data() {
         return {
-            categories: [
-                {
-                    title: 'Social Links',
-                    links: [
-                        { 
-                            name: 'Telegram', 
-                            url: 'https://t.me/yourchannel', 
-                            icon: 'https://cdn.simpleicons.org/telegram/0088cc', 
-                            iconType: 'simple',
-                            color: '#0088cc', 
-                            description: 'Join our Telegram channel for updates and discussions.' 
-                        },
-                        { 
-                            name: 'X', 
-                            url: 'https://x.com/librelol', 
-                            icon: 'https://cdn.simpleicons.org/x/FFFFFF', 
-                            iconType: 'simple',
-                            color: '#FFFFFF', 
-                            description: 'Follow us on X for news and announcements.' 
-                        },
-                    ],
-                },
-                {
-                    title: 'Proxies',
-                    links: [
-                        { 
-                            name: 'Searx', 
-                            url: 'https://search.libre.lol', 
-                            icon: 'https://cdn.simpleicons.org/searxng/0088cc', 
-                            iconType: 'simple',
-                            color: '#0088cc', 
-                            description: 'Search the web privately with Searx.' 
-                        },
-                        { 
-                            name: 'Invidious', 
-                            url: 'https://inv.libre.lol', 
-                            icon: 'mdi-play-box',
-                            iconType: 'mdi',
-                            color: '#FF0000', 
-                            description: 'Watch YouTube videos securely without ads.' 
-                        },
-                        {
-                            name: 'Wikiless',
-                            url: 'https://wikiless.libre.lol',
-                            icon: 'mdi-wikipedia',
-                            iconType: 'mdi',
-                            color: '#000000',
-                            description: 'Access Wikipedia articles without tracking.'
-                        }
-                    ],
-                },
-                {
-                    title: 'Recommended Services',
-                    links: [
-                        { 
-                            name: 'Mullvad VPN', 
-                            url: 'https://mullvad.net', 
-                            icon: 'mdi-vpn', 
-                            iconType: 'mdi',
-                            color: '#008000', 
-                            description: 'A trusted VPN provider known for privacy and security.' 
-                        },
-                        { 
-                            name: 'Proton Mail', 
-                            url: 'https://proton.me/mail', 
-                            icon: 'https://cdn.simpleicons.org/protonmail/8B89CC', 
-                            iconType: 'simple',
-                            color: '#8B89CC', 
-                            description: 'Secure and encrypted email for privacy-conscious users.' 
-                        },
-                        {
-                            name: 'Telegraph',
-                            url: 'https://telegra.ph',
-                            icon: 'mdi-newspaper',
-                            iconType: 'mdi',
-                            color: '#0088cc',
-                            description: 'Publish articles and stories anonymously.'
-                        }
-                    ],
-                },
-                {
-                    title: 'Tools',
-                    links: [
-                        { 
-                            name: 'Private Bin', 
-                            url: 'https://paste.libre.lol', 
-                            icon: 'mdi-file-document', 
-                            iconType: 'mdi',
-                            color: '#000000', 
-                            description: 'An open-source paste bin alternative focused on privacy.' 
-                        },
-                        { 
-                            name: 'Git', 
-                            url: 'https://git.libre.lol', 
-                            icon: 'mdi-git', 
-                            iconType: 'mdi',
-                            color: '#F05032', 
-                            description: 'A self-hosted Git service for the public.' 
-                        },
-                        { 
-                            name: 'Vaultwarden', 
-                            url: 'https://pass.libre.lol', 
-                            icon: 'mdi-lock', 
-                            iconType: 'mdi',
-                            color: '#FFD700', 
-                            description: 'Manage your passwords securely with this Bitwarden alternative.' 
-                        },
-                    ],
-                },
-                {
-                    title: "Notable Mentions",
-                    links: [
-                        {
-                            name: 'UNSPA',
-                            url: 'https://unspa.com',
-                            icon: 'mdi-school',
-                            iconType: 'mdi',
-                            color: '#FF0000',
-                            description: 'American Nationalist Group'
-                        }
-                    ],
-                }
-            ],
+            categories,
+            searchQuery: '', // Added search query
         };
+    },
+    computed: {
+        filteredCategories() {
+            // Filter categories based on search query in titles, links, and descriptions
+            return this.categories.map(category => ({
+                ...category,
+                links: category.links.filter(link =>
+                    link.name.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+                    link.description.toLowerCase().includes(this.searchQuery.toLowerCase())
+                )
+            })).filter(category => category.links.length > 0); // Only include categories with matching links
+        },
     },
 }
 </script>
 
 <style scoped>
-.v-card {
-    border-radius: 12px;
-    transition: transform 0.3s, box-shadow 0.3s;
+.search-bar {
+    max-width: 400px; /* Set a max width for the search bar */
+    width: 100%; /* Full width */
+    border-radius: 24px; /* Rounded corners */
 }
 
-.v-card:hover {
+.scrollable {
+    overflow-x: auto; /* Enable horizontal scrolling */
+    white-space: nowrap; /* Prevent wrapping of columns */
+}
+
+.formatted-card {
+    border-radius: 12px;
+    transition: transform 0.3s, box-shadow 0.3s;
+    width: 400px; /* Standard width */
+    margin: 16px; /* Spacing between cards */
+}
+
+.formatted-card:hover {
     transform: scale(1.05);
     box-shadow: 0 6px 30px rgba(0, 0, 0, 0.2);
 }
 
-.v-list-item {
+.link-item {
     transition: background-color 0.2s;
 }
 
-.v-list-item:hover {
+.link-item:hover {
     background-color: rgba(255, 255, 255, 0.1);
-}
-
-.list-item-animated {
-    transition: transform 0.2s;
-}
-
-.list-item-animated:hover {
-    transform: translateX(5px);
 }
 
 .icon {
@@ -219,5 +132,14 @@ export default {
 
 .font-weight-semibold {
     font-weight: 600;
+}
+
+/* Media Queries */
+@media (max-width: 768px) {
+    .scrollable {
+        overflow-x: hidden; /* Disable scrolling on mobile */
+        display: flex; /* Allow cards to expand */
+        flex-wrap: wrap; /* Wrap items */
+    }
 }
 </style>
